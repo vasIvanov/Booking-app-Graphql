161576b24bf52b6f93078fbdfde7b8584cee91d7
Vasil Ivanov <vasivanov.info@gmail.com>
2020-07-27 10:53:26 +0300

    update

---
+++ public/build/precache-manifest.af4f7aa391dd9325f17ac19b205b3c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e088eb2d97f5918d15c1450726562469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e910c23fb758067cb80a",
+    "url": "/static/css/main.b2154600.chunk.css"
+  },
+  {
+    "revision": "80561b3500412432bb22",
+    "url": "/static/js/2.52de874c.chunk.js"
+  },
+  {
+    "revision": "f20aefe218d6b29985aa161bf7fd203e",
+    "url": "/static/js/2.52de874c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e910c23fb758067cb80a",
+    "url": "/static/js/main.7de013d9.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
